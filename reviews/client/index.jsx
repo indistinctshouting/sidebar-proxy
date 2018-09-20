f4d42ee35f64a3b3726c6fba4f6c8d9c94d033fa
jeongle <jyjjeong@gmail.com>
2018-09-20 14:51:07 -0700

    removed all client folders

--- reviews/client/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Reviews from './components/App.jsx';
-
-ReactDOM.render(<Reviews/>, document.getElementById('reviews-ratings'));
--- sidebar/client/src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Sidebar from './components/App.jsx';
-
-ReactDom.render(<Sidebar />, document.getElementById('sidebar'));
