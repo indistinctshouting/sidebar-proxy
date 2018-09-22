507b1c45ddfb180e376c56e5004d267cc270e9af
jeongle <jyjjeong@gmail.com>
2018-09-21 18:45:28 -0700

    removed all module folders

--- sidebar/db/MONGOOSE_KEY_empty.js
+++
@@ -1,3 +0,0 @@
-const MONGOOSE_KEY = 'Fill In Mlab credentials (ie mongodb://<dbuser>:<dbpassword>@blahblah.mlab.com:blahblah)';
-// Make separate file called MONGOOSE_KEY.js with the contents of this file
-module.exports.MONGOOSE_KEY = MONGOOSE_KEY;
